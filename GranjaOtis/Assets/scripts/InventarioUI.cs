a44660e0fee934e4d6c7a1fe1023b50ab1e0b177
DianaSoy02 <diana.agustin@ucic.edu.mx>
2025-07-28 16:53:32 -0600

    v9

---
+++ GranjaOtis/Assets/scripts/InventarioUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InventarioUI : MonoBehaviour
+
+
+
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      private int huevosPrevios = -1; 
+	private Label labelHuevos; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
